de18767653b7ec284f28ea1ad36c6152c0ec64ea
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-06-02 11:06:26 +0000

    DuckNumber

--- Main.java
+++ Main.java
@@ -8,9 +8,23 @@ class Main{
         int num = input.nextInt();
         boolean result = EvenOdd.isNatural(num);
 
-        if (result) {
-            boolean buzz = BuzzNumber.isBuzz(num);
-        }
+
+        if (result){
+            boolean isEven = EvenOdd.isEvenOdd(num);
+            boolean isBuzz = BuzzNumber.isBuzz(num);
+            boolean isDuck = DuckNumber.isDuck(num);
+            String output = String.format("""
+                   Properties of %d
+                   even: %b
+                    odd: %b
+                   buzz: %b
+                   duck: %b
+                    """, num, isEven, !isEven, isBuzz, isDuck);
+            System.out.println(output);
+        }else System.out.println("This number is not natural!");
+                    
+                
+        
         
 
         
