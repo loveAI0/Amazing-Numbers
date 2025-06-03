5baf0e50b04b6e2f7926c64d3b4f40101bdaf606
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-06-03 09:10:57 +0000

    SPY NUMBER PROPERTIES IS ADDED.

--- GapfulNumber.java
+++ GapfulNumber.java
@@ -2,14 +2,12 @@ public class GapfulNumber {
     static boolean isGapful(long num){
         String strNum = String.valueOf(num);
         int length = strNum.length();
-
+        if(length == 1 || length == 2) return false;
         // extracting first and last
-        String firstLastNum = "" + strNum.charAt(0) + strNum.charAt(length - 1);
+        String firstLastNum = "" + strNum.charAt(0) +""+ strNum.charAt(length - 1);
 
         
         return num % Long.parseLong(firstLastNum) == 0;
     }
-    public static void main(String[] args) {
-        System.out.println(isGapful(7880));
-    }
+    
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 class Main{
     public static void main(String [] args){
         
@@ -13,13 +15,14 @@ class Main{
                 - enter a natural number to know its properties;
                 - enter two natural numbers to obtain the properties of the list:
                 * the first parameter represents a starting number;
-                * the second parameter shows how many consecutive numbers are to be processed;
+                * the second parameter shows how many consecutive numbers are to be printed;
+                - two natural numbers and a property to search for;
                 - separate the parameters with one space;
                 - enter 0 to exit.""");
        
         while(true){
             //input
-            System.out.print("Enter a request: ");
+            System.out.println("Enter a request: ");
             //either one or two values
             String numStr = input.nextLine();
             String[] arr1 = numStr.split(" ");
@@ -48,6 +51,7 @@ class Main{
                         if (DuckNumber.isDuck(j)) sb.append("duck ");
                         if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
                         if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                        if(SpyNumber.isSpy(j)) sb.append("spy ");
                         if (EvenOdd.isEvenOdd(j)) sb.append("even ");
                         else sb.append("odd ");
 
@@ -60,7 +64,7 @@ class Main{
                 
                 } 
             }
-            else{
+            else if(arr1.length == 1){
                 long[] arr = new long[1];
                 arr[0] = Long.parseLong(arr1[0]);
                 boolean result = EvenOdd.isNatural(arr[0]);
@@ -70,15 +74,17 @@ class Main{
                     boolean isDuck = DuckNumber.isDuck(arr[0]);
                     boolean isPalindrom = PalindromicNumber.isPalindrom(arr[0]);
                     boolean isGapful = GapfulNumber.isGapful(arr[0]);
+                    boolean isSpy = SpyNumber.isSpy(arr[0]);
                     String output = String.format("""
                             Properties of %,d
                                     buzz: %b
                                     duck: %b
                              palindromic: %b
                                   gapful: %b
+                                     spy: %b
                                     even: %b
                                      odd: %b
-                                """, arr[0],isBuzz, isDuck, isPalindrom,isGapful,isEven, !isEven);
+                                """, arr[0],isBuzz, isDuck, isPalindrom,isGapful,isSpy,isEven, !isEven);
                     System.out.println(output);
                 }
                 else{
@@ -86,6 +92,153 @@ class Main{
                 
                 } 
                 
+            }
+            else{
+                // number times and properties
+                
+                ArrayList <String> properties = new ArrayList<>(Arrays.asList("BUZZ", "DUCK", "PALINDROMIC", "GAPFUL", "SPY", "EVEN", "ODD"));
+
+                long[] arr = new long[2];
+                arr[0] = Long.parseLong(arr1[0]);
+                arr[1] = Long.parseLong(arr1[1]);
+                // arr1 is input string
+                if(!properties.contains(arr1[2].toUpperCase())){
+                    System.out.println(String.format("""
+                            The property [%s] is wrong.
+                            Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]
+                            """, arr1[2]));
+                    break;
+                }
+                
+                boolean result = EvenOdd.isNatural(arr[0]);
+                
+
+                if (result){
+                    long j = arr[0];
+                    int times = (int) arr[1];
+                    int k = 0;
+                   
+                    while(k != times){
+                         StringBuilder sb = new StringBuilder();
+                       
+                        if (arr1[2].equals("EVEN") && EvenOdd.isEvenOdd(j)) {
+                            
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                    
+                            j += 2;
+                            k++;
+                        }
+                        else if (arr1[2].equals("ODD") && EvenOdd.isEvenOdd(j)) {
+                            
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                            j++;
+                            k++;
+                        }
+                        else if (arr1[2].equals("BUZZ")&& BuzzNumber.isBuzz(j)) {
+                           
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                            j++;
+                        }
+                        else if (arr1[2].equals("GAPFUL") && GapfulNumber.isGapful(j)) {
+                           
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                            j++;
+                        }
+                        else if (arr1[2].equals("PALINDROMIC") && PalindromicNumber.isPalindrom(j)) {
+                           
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                            j++;
+                            k++;
+                        }
+                        else if (arr1[2].equals("DUCK")&& DuckNumber.isDuck(j)) {
+                            
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                            j++;
+                            k++;
+                        }
+                        else if(arr1[2].equals("SPY") && SpyNumber.isSpy(j)) {
+                            
+                            sb.append(String.format("%,d", j)).append(" is ");
+
+                            if (BuzzNumber.isBuzz(j)) sb.append("buzz ");
+                            if (DuckNumber.isDuck(j)) sb.append("duck ");
+                            if (PalindromicNumber.isPalindrom(j)) sb.append("palindromic ");
+                            if (GapfulNumber.isGapful(j)) sb.append("gapful ");
+                            if(SpyNumber.isSpy(j)) sb.append("spy ");
+                            if (EvenOdd.isEvenOdd(j)) sb.append("even ");
+                            else sb.append("odd ");
+                            System.out.println(sb.toString().trim()); // Removes extra spaces
+                            j++;
+                            k++;
+                            
+                        }
+                        else{
+                            j++;
+                        }
+                        
+                        
+                    }
+                        
+                }
+
+                else{
+                    System.out.println("The first parameter should be a natural number or zero.");
+                
+                }
             }
         }       
     }
